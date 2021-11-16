ffc4543d9d9fbf8978b36a75ab60cfa3bdd9a322
Kang-Gumsil <49671565+Kang-Gumsil@users.noreply.github.com>
2021-11-16 15:51:50 +0900

    깃 구조 변경

--- 소프티어 오픈이벤트/01 바이러스.py
+++
@@ -1,4 +0,0 @@
-k, p, n = map(int, input().split())
-
-result = k * (p ** n)
-print(result % 1000000007)
--- 이것이 코딩테스트다 with 파이썬/chap10/10-2 경로 압축 기법.py
+++
@@ -1,9 +0,0 @@
-# def find_parent(parent, x):
-#     if parent[x] != x:
-#         return find_parent(parent, parent[x])
-#     return x
-
-def find_parent(parent, x):
-    if parent[x] != x:
-        parent[x] = find_parent(parent, parent[x])
-    return parent[x]
